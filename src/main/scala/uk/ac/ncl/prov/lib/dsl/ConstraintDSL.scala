896fbc5fb0aa7f39009640d99a075d0fc8500d67
Hugo Firth <hebfirth@gmail.com>
2013-12-03 18:17:44 +0000

    Tidied up some errors and added some TODO statements so that I can dive back in tomorrow.

--- src/main/scala/uk/ac/ncl/prov/lib/dsl/ConstraintDSL.scala
+++ src/main/scala/uk/ac/ncl/prov/lib/dsl/ConstraintDSL.scala
@@ -11,12 +11,14 @@ object ConstraintDSL extends StandardTokenParsers {
     lexical.reserved += ("an", "a", "the", "must", "may", "have", "be", "when", "unless")
 
     def constraint: Parser[Constraint] = determiner~imperative~condition^^{
-      case d~i~c => new Constraint(Constraint.Type.ACTIVITY) //TODO actually work out constructor
+      case d~i~c => new Constraint(d, i, c) //TODO actually work out constructor
     }
     //Get the determined object or type to which a constraint applies
     def variableDeterminer = ("a" | "an")
     def inVariableDeterminer = "the"
-    def determiner: Parser[String] = variableDeterminer ~> "(" ~> stringLit <~ ")"
+    //TODO: Workout easiest method of String -> Enum type matching
+    //TODO: Workout neatest method of checking for / coping with invariable determiner
+    def determiner: Parser[Determiner] = variableDeterminer ~> "(" ~> stringLit <~ ")"^^{case s => new Determiner(PROV.Type.Activity)}
 
 
     //
