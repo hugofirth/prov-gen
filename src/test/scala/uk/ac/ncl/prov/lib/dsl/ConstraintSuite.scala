784e0e9df9ceb564e7aa7c3d33c7b7ace09cf824
Hugo Firth <hebfirth@gmail.com>
2013-12-12 11:44:28 +0000

    Weird bracket issues cleared up - tests passing ... again

--- src/test/scala/uk/ac/ncl/prov/lib/dsl/ConstraintSuite.scala
+++ src/test/scala/uk/ac/ncl/prov/lib/dsl/ConstraintSuite.scala
@@ -2,6 +2,7 @@ package uk.ac.ncl.prov.lib.dsl
 
 import org.scalatest.FunSuite
 import org.scalatest.matchers._
+import prov._, degree._
 
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -16,21 +17,23 @@ class ConstraintSuite extends FunSuite with ShouldMatchers{
   }
 
   test("a valid constraint string should parse without throwing an exception") {
-    evaluating { ConstraintDSL.parseDSL("an(Agent).must(have(in.degree().at.most(3)).unless(it.has(relationship(ActedOnBehalfOf).exactly(1))") }
-    evaluating { ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy).at.least(1)).when(it.has.relationship(WasAttributedTo).at.least(1))") }
+    evaluating { ConstraintDSL.parseDSL("an(Agent).must(have(in.degree()).at.most(3)).unless(it.has(relationship(ActedOnBehalfOf)).exactly(1))") }
+    evaluating { ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy)).at.least(1)).when(it.has(relationship(WasAttributedTo)).at.least(1))") }
   }
 
   //Test Determiner
   test("a valid constraint string should correctly identify an invariable determiner") {
-    val result = ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy).at.least(1)).when(it.has.relationship(WasAttributedTo).at.least(1))")
+    val result = ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy)).at.least(1)).when(it.has(relationship(WasAttributedTo)).at.least(1))")
     result.determiner.invariable should equal(true)
     result.determiner.identifier should (not be null and equal("e1"))
   }
 
   //Test Imperative
   test("a valid constraint string should correctly parse and determine an imperative") {
-    val result = ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy).at.least(1)).when(it.has.relationship(WasAttributedTo).at.least(1))")
-
+    val result = ConstraintDSL.parseDSL("the(Entity, e1).may(have(relationship(WasGeneratedBy)).at.least(1)).when(it.has(relationship(WasAttributedTo)).at.least(1))")
+    result.imperative.necessary should equal(false)
+    result.imperative.requirement.isInstanceOf[RelationshipRequirement] should equal(true)
+    result.imperative.requirement.asInstanceOf[RelationshipRequirement].relation should equal(Relation.WASGENERATEDBY)
   }
 
   //Test Condition
